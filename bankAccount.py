ca8208924e2f1f7b33d8be22dc0816e1b0ed6d3b
Pablo Sepulveda <p.sepulvedamorande@gmail.com>
2022-01-23 15:47:47 -0300

    BankAccount 2.0, faltaba la segunda cuenta

--- bankAccount.py
+++ bankAccount.py
@@ -17,7 +17,7 @@ class BankAccount:
 
     def display_account_info(self):
         print(f"Banco {self.bank} le informa que el Balance de la cuenta {self.type} numero {self.account_number} de {self.account_owner} es de {self.account_balance} Dolares")
-        print(f"Los interes anuales a fin de año de esta cuanta rondaran los {self.account_balance*self.interes} Dolares")
+        print(f"Los interes anuales a fin del periodo de esta cuanta rondaran los {self.account_balance*self.interes} Dolares")
         return self
 
     def yield_interest(self):
@@ -26,9 +26,12 @@ class BankAccount:
 
 
 ps123456 = BankAccount ("Pablo Sepulveda", 123456, "Santander", "corriente",0.005)
+mr789101 = BankAccount ("Madalena Rolando", 789101, "Santander Prime", "corriente Premium",0.009)
 
-ps123456.deposit(1000).deposit(1300).withdraw(200).deposit(825).deposit(100).withdraw(90).yield_interest()
+ps123456.deposit(1000).deposit(1300).withdraw(200).deposit(825).yield_interest()
+mr789101.deposit(5000).deposit(11300).withdraw(200).withdraw(800).withdraw(3000).withdraw(500).yield_interest()
 
 ps123456.display_account_info()
+mr789101.display_account_info()
 
 
