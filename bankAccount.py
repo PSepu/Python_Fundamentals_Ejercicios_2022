386fa4c278e0787848dc68cad108d69da8121198
Pablo Sepulveda <p.sepulvedamorande@gmail.com>
2022-01-23 15:40:56 -0300

    BankAccount

---
+++ bankAccount.py
@@ -0,0 +1,34 @@
+class BankAccount:
+    def __init__(self,account_owner,account_number,bank,tipo,interes):
+        self.account_owner = account_owner
+        self.account_number = account_number
+        self.bank = bank
+        self.type = tipo
+        self.account_balance=0
+        self.interes = interes
+
+    def deposit(self,amount):
+        self.account_balance += amount
+        return self
+
+    def withdraw(self,amount):
+        self.account_balance -= amount
+        return self
+
+    def display_account_info(self):
+        print(f"Banco {self.bank} le informa que el Balance de la cuenta {self.type} numero {self.account_number} de {self.account_owner} es de {self.account_balance} Dolares")
+        print(f"Los interes anuales a fin de año de esta cuanta rondaran los {self.account_balance*self.interes} Dolares")
+        return self
+
+    def yield_interest(self):
+        self.account_balance += self.account_balance*self.interes
+        return self
+
+
+ps123456 = BankAccount ("Pablo Sepulveda", 123456, "Santander", "corriente",0.005)
+
+ps123456.deposit(1000).deposit(1300).withdraw(200).deposit(825).deposit(100).withdraw(90).yield_interest()
+
+ps123456.display_account_info()
+
+
